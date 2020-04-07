35dd5e5060ab72afb0a5b119c52729f1032e9940
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-07 17:16:06 -0600

    Resolviendo bugs

--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -40,7 +40,7 @@ export default function LayoutAdmin(props) {
             <LoadRoutes routes={routes} />
           </Content>
           <Footer className="layout-admin__footer">
-            Agustin Navarro Galdon
+           Michael Soro Zúñiga
           </Footer>
         </Layout>
       </Layout>
