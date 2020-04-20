2e5befea9ceac20441792a36e067dcc517edfad7
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-19 19:57:32 -0600

    Añadiendo la funcionalidad para eliminar usuario en panel de admin y el Formulario para crear nuevos usuarios con su función

--- src/api/user.js
+++ src/api/user.js
@@ -198,3 +198,49 @@ export function activateUserApi(token, userId, status) {
     return err.message; 
   })
 }
+
+/*-----------------------------*/
+/* Borrar usuario */
+/*-----------------------------*/
+export function deleteUserApi(token, userId) {
+  const url = `${basePath}/${apiVersion}/delete-user/${userId}`;
+
+  const params = {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: token
+    }
+  };
+
+    return fetch(url, params)
+      .then(response => {
+      return response.json();
+      }).then(result => {
+        return result.message;
+      }).catch(err => {
+        return err.message; 
+      });
+}
+
+export function signUpAdminApi(token, data) {
+  const url = `${basePath}/${apiVersion}/sign-up-admin`;
+
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: token
+    },
+    body: JSON.stringify(data)
+  }
+
+  return fetch(url, params).then(response => {
+    return response.json();
+  }).then(result => {
+    return result.message;
+  })
+  .catch(err => {
+    return err.message;
+  });
+}
--- src/components/Admin/Users/ListUsers/ListUsers.js
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Switch,List,Avatar,Button,Icon,notification } from 'antd';
+import { Switch,List,Avatar,Button,Icon,notification, Modal as ModalAntd } from 'antd';
 import NoAvatar from '../../../../assets/img/png/no-avatar.png';
 import Modal from '../../../Modal';
 import EditUserForm from '../EditUserForm';
+import AddUserForm from '../AddUserForm';
 
-import {getAvatarApi, activateUserApi } from '../../../../api/user';
+import {getAvatarApi, activateUserApi, deleteUserApi } from '../../../../api/user';
 import {getAccessTokenApi} from '../../../../api/auth';
 
 import './ListUsers.scss';
 
+const { confirm } =  ModalAntd;
 export default function ListUsers(props) {
     const {usersActive, usersInactive, setReloadUsers} = props;
     const [viewUserActives, setViewUsersActives] = useState(true);
@@ -18,19 +20,31 @@ export default function ListUsers(props) {
     
     // console.log(usersActive);
     // console.log(usersInactive);
-
+    const  addUserModal = () => {
+        setIsVisibleModal(true);
+        setModalTitle("Creando nuevo usuario");
+        setModalContent(
+                <AddUserForm setIsVisibleModal={setIsVisibleModal} setReloadUsers={setReloadUsers}/>
+        );   
+    };
 
     return (
         <div className="list-users">
-           <div className="list-users__switch">
-           <Switch  defaultChecked
-                    onChange={() => setViewUsersActives(!viewUserActives)}
-                    
-            />
-            <span>
-                {viewUserActives ? "Usuarios Activos" : "Usuarios Inactivos"}
-            </span>
-           </div>
+            <div className="list-users__header">
+                <div className="list-users__header-switch">
+                    <Switch  defaultChecked
+                            onChange={() => setViewUsersActives(!viewUserActives)}
+                            
+                    />
+                        <span>
+                            {viewUserActives ? "Usuarios Activos" : "Usuarios Inactivos"}
+                        </span>
+                </div>
+                <Button type="primary" onClick={addUserModal}>Nuevo usuario</Button>
+        
+            </div>
+            
+
            {viewUserActives ? (
                 <UsersActive usersActive={usersActive}
                             setIsVisibleModal={setIsVisibleModal} 
@@ -104,7 +118,34 @@ function UserActive(props) {
                 message:err
             });
         });
-    }
+    };
+
+    /*-----------------------------*/
+    /* Mostrar el modal si desea eliminar usuario activo */
+    /*-----------------------------*/
+    const showDeleteConfirm = () => {
+        const  accessToken = getAccessTokenApi();
+        confirm({
+            title: "Eliminando usuario",
+            content: `¿Estas seguro que quieres eliminar a ${user.email} ?`,
+            okText : "danger",
+            cancelText: "Cancelar",
+            onOk() {
+                deleteUserApi(accessToken, user._id)
+                .then(response => {
+                    notification["success"]({
+                        message: response
+                    });
+                    setReloadUsers(true);
+                })
+                .catch(err => {
+                    notification["error"]({
+                        message: err
+                    });
+                })
+            }
+        });
+    };
     return (
         <List.Item actions={[
 
@@ -114,7 +155,7 @@ function UserActive(props) {
                 <Button type="danger" onClick={desactivateUser}>
                     <Icon type="stop"/>
                 </Button>,
-                    <Button type="danger" onClick={() => console.log("Eliminar usuario")}>
+                    <Button type="danger" onClick={showDeleteConfirm}>
                         <Icon type="delete"/>
                     </Button>
         ]}>
@@ -176,12 +217,38 @@ function UserInactive(props) {
             });
         });
   };
+   /*-----------------------------*/
+    /* Mostrar el modal si desea eliminar usuario inactivo */
+    /*-----------------------------*/
+    const showDeleteConfirm = () => {
+        const  accessToken = getAccessTokenApi();
+        confirm({
+            title: "Eliminando usuario",
+            content: `¿Estas seguro que quieres eliminar a ${user.email} ?`,
+            okText : "danger",
+            cancelText: "Cancelar",
+            onOk() {
+                deleteUserApi(accessToken, user._id)
+                .then(response => {
+                    notification["success"]({
+                        message: response
+                    });
+                    setReloadUsers(true);
+                })
+                .catch(err => {
+                    notification["error"]({
+                        message: err
+                    });
+                })
+            }
+        });
+    };
   return (
     <List.Item actions={[
         <Button type="primary" onClick={activateUser}>
                 <Icon type="check" />
         </Button>,
-             <Button type="danger" onClick={() => console.log("Eliminar usuario")}>
+             <Button type="danger" onClick={showDeleteConfirm}>
                  <Icon type="delete"/>
             </Button>
     ]}>
