c01f7029fd1b848bf01e2e881541c5b92378f879
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-20 09:18:00 -0600

    Listando Menús con acción del drag and drop

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,15 +1,74 @@
 import React, {useState, useEffect} from 'react';
-
+import {Switch, List, Button, Icon, Modal as ModalAntd, notification} from 'antd';
+import Modal from  '../../../Modal';
+import DragSortableList from 'react-drag-sortable'
 import "./MenuWebList.scss";
+
+
+const {confirm} = ModalAntd;
+
+/*-----------------------------*/
+/* Listando menús con acción de drag and drop */
+/*-----------------------------*/
 export default  function MenuWebList(props) {
     const { menu, setReloadMenuWeb } = props;
-    
-    return(
-        <div>
-            <h1>MenuWebList....</h1>
-            {menu.map(item => (
-                <p key={item._id}>{item.title}</p>
-            ))}
+
+    /*-----------------------------*/
+    /* Estado que va guardar la nueva version del menu */
+    /*-----------------------------*/
+    const [listItems, setListItems] = useState([]);
+
+    /*-----------------------------*/
+    /* Estados del menu*/
+    /*-----------------------------*/
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [modalTitle, setModalTtile] = useState(""); 
+    const [modalContent, setModalContent] = useState(null);
+
+
+    useEffect(() => {
+        const listItemArray = [];
+        menu.forEach(item => {
+            listItemArray.push({
+                content: (<MenuItem item={item}/>)
+            })
+        });
+        setListItems(listItemArray);
+    }, [menu])
+
+    const OnSort  = (sortedList, dropEvent) => {
+            console.log(sortedList);
+    };
+
+    return (
+        <div className="menu-web-list">
+            <div className="menu-web-list__header">
+                <Button type="primary">Menu menú</Button>
+            </div>
+            <div className="menu-web-list__items">
+                <DragSortableList items={listItems} OnSort={OnSort} type="vertical"/>
+            </div>
         </div>
     );
+}
+
+function MenuItem(props) {
+const {item} = props;
+
+return (
+    <List.Item 
+            actions={[
+
+                 //Switch para activar el menu 
+                <Switch defaultChecked={item.active}/>,
+                    <Button type="primary">
+                        <Icon type="edit"/>
+                    </Button>,
+                         <Button type="danger">
+                             <Icon type="delete"/>
+                        </Button>
+            ]}>
+                <List.Item.Meta title={item.title} description={item.url}/>
+    </List.Item>
+)
 }
