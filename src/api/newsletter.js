7395bc758c690e67acdf15d2b902438aa983db39
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-21 13:08:20 -0600

    Creando la estructura y formulario de la Newsletter y conectando formulario con el endpoint que registra emails

---
+++ src/api/newsletter.js
@@ -0,0 +1,22 @@
+import { basePath, apiVersion } from "./config";
+
+/*-----------------------------*/
+/* Funcion para suscribir el email desde la api */
+/*-----------------------------*/
+export function suscribeNewsletterApi(email) {
+  const url = `${basePath}/${apiVersion}/suscribe-newsletter/${email.toLowerCase()}`;
+  const params = {
+    method: "POST"
+  };
+
+  return fetch(url, params)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      return err;
+    });
+}
