3694eefaa139aba22e8fbca2a1041889b9633342
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-22 13:27:43 -0600

    Se creo  la estructura básica de la sección del blog

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Modal as ModalAntd } from 'antd';
 
 export default function Modal(props) {
-const {children, title, isVisible, setIsVisible} = props;
+const {children, title, isVisible, setIsVisible, ...other} = props;
 
 return(
-    <ModalAntd title={title} centered visible={isVisible} onCancel={() => setIsVisible(false)} footer={false}>
+    <ModalAntd title={title} centered visible={isVisible} onCancel={() => setIsVisible(false)} footer={false} {...other}>
         {children}
     </ModalAntd>
 )
--- src/pages/Admin/Blog/Blog.js
+++ src/pages/Admin/Blog/Blog.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
+import { Button, notification } from "antd";
+import Modal from "../../../components/Modal";
 import "./Blog.scss";
+
+
 export default function Blog() {
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState("");
+    const [modalContent , setModalContent] = useState(null);
+
     return (
-        <div>
-            <h1>Blog...</h1>
+        <div className="blog">
+           <div className="blog__add-post">
+            <Button type="primary">
+                 Nuevo post
+            </Button>
+           </div>
+
+            <h1>Post list...</h1>
+            <h2>Paginación</h2>
+           <Modal title={modalTitle}
+                  isVisible={isVisibleModal}
+                  setVisible={setIsVisibleModal}
+                  with="75%"
+            />
         </div>
-    )
+    );
 }
