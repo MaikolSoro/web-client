0b8ca08179a031bdd8a6d8fd37ab51ba0304f098
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-23 22:35:29 -0600

    Cambio de icono de l app

--- src/components/Admin/MenuSider/MenuSider.js
+++ src/components/Admin/MenuSider/MenuSider.js
@@ -10,12 +10,12 @@ import "./MenuSider.scss";
   return (
     <Sider className="menu-sider" collapsed={menuCollapsed}>
       <Menu theme="dark" mode="inline" defaultSelectedKeys={[location.pathname]}>
-        <Menu.Item key="/admin">
-          <Link to={"/admin"}>
+       {/* <Menu.Item key="/admin">
+          <Link to="/admin">
             <Icon type="home" />
             <span className="nav-text">Home</span>
           </Link>
-        </Menu.Item>
+        </Menu.Item> */}
         <Menu.Item key="/admin/users">
           <Link to={"/admin/users"}>
             <Icon type="user" />
